fd764d0e53c18eeda86beafd2f7fa144dc6b358a
gebaor <144036222+gebaor@users.noreply.github.com>
2024-03-06 15:32:44 +0800

    摊位管理
    摊位管理

---
+++ Source/administrator/Login screen/vue-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+<emp-view></emp-view>
+  </div>
+</template>
+
+<script>
+//import ElementView from './views/element/ElementView.vue';
+import EmpView from './views/tlias/EmpView.vue';
+export default {
+  components: {  EmpView },
+  data() {
+    return {
+      message: "hello vue",
+    }
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
